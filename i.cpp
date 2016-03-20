0e6c216cce01ed747291692cb32ac137a51fcb41
l1254879 <l1254879@gmail.com>
2016-03-21 01:16:09 +0800

    use get line

--- i.cpp
+++ i.cpp
@@ -5,39 +5,44 @@
 #include<cstdlib>
 using namespace std;
 
-void open_file();
+void open_file(int,char**);
 int sumofAllPlaces(const string &IDNumber);
 bool isvalid(const string &IDNumber);
 int returnSubstr(const string &IDNumber);
 void allinone(const string &IDNumber,bool type);
-void generate(char*);
-int main(){
-  struct timeval tv;
-  struct timeval tz;
-  gettimeofday(&tv,NULL);
-  open_file();
-  gettimeofday(&tz,NULL);
-  long spentus=(tz.tv_sec*1000000+tz.tv_usec)-(tv.tv_sec*1000000+tv.tv_usec);
-  int us=spentus%1000000;
-  cout<<us<<" microseconds"<<endl;
+void generate(int);
+
+int main(int argc,char*argv[]){
+  open_file(argc,argv);
   return 0;
 }
-void open_file(){
+void open_file(int argc,char *argv[]){
   FILE*tmp;
-  char id[11]={0};//since fgets read \0
+  char id[10]={0};//since fgets read \0
 	bool type;
+	int i;
   fstream file;
-  tmp=fopen("id_out.txt","r");
-  if(!tmp){ //if faile to open file
+  if(argc==1){ //if faile to open file
     cout<<"Fail to open file"<<endl;
   }
-  else{
-    fgets(id,11,tmp);//get the a line string
+ else if(argc==2){
+  	file.open(argv[1],ios::in);
+  	do{//get a line string
+  		file.getline(id,sizeof(id));
+		type=isvalid(id);
+			if(type== true)
+ 				allinone(id,type);
+			else
+ 				allinone(id,type);
+
+	}while(!file.eof());
+	file.close();
   }
- type=sumofAllPlaces(id);
-	allinone(id,type);
-
-
+  else{
+  	cout<<"Too much argument"<<endl;
+  }
+  
+ 
 }
 bool isvalid(const string &IDNumber){
 	int check;//get the check code
@@ -51,7 +56,7 @@ bool isvalid(const string &IDNumber){
 }
 
 int sumofAllPlaces(const string &IDNumber){
-	  int lo_num;//get the local stat change code
+		int lo_num;//get the local stat change code
 		int rest;//aim of lo_num
 		int divide;//aim of lo_num
 		int check;
@@ -71,6 +76,7 @@ int sumofAllPlaces(const string &IDNumber){
 }
 
 bool startsWith(const string &IDNumber,const string &substr){
+	//if(substr)
 
 }
 string& fakeOne(const string &gender){
@@ -84,29 +90,47 @@ int returnSubstr(const string &IDNumber){
 	return change[get_ascii];
 }
 void allinone(const string &IDNumber,bool type){
-	char Id[10]={0};
-	if(type)
-		cout<<IDNumber<<": an invalid ID #"<<endl;
-	else
-		cout<<IDNumber<<": an invalid ID #"<<endl;
-	generate(Id);
-
+	char gender;
+  struct timeval tv;
+  struct timeval tz;
+	if(sizeof(IDNumber)>1){
+	
+		if(type)
+			cout<<IDNumber<<": an valid ID #";
+		else
+			cout<<IDNumber<<": an invalid ID #";
+	}
+	else{
+			if(IDNumber[0]=='M')
+				gender='M';
+			else if(IDNumber[0]=='F')
+				gender='F';
+			cout<<"This is gender"<<gender<<endl;
+			//generate(gender);
+		}
+  		/*gettimeofday(&tv,NULL);
+		generate(gender);
+  		gettimeofday(&tz,NULL);
+ 		 long spentus=(tz.tv_sec*1000000+tz.tv_usec)-(tv.tv_sec*1000000+tv.tv_usec);
+  		int us=spentus%1000000;
+		cout<<"Timing:"<<us<<" microseconds"<<endl;*/
 
 }
-void generate(char *IDNumber){
+void generate(char gender){
 	char ran;
-	int i;
+	char Id[11]={NULL};
 	char english;
-	int check;
+	int check,i;
 	srand(time(NULL));
 	english=rand()%26+65;
-	IDNumber[0]=english;
+	Id[0]=english;
+	Id[1]=gender;
 	for(i=2;i<9;i++){
 		ran=rand()%10+48;
-		IDNumber[i]=ran;
+		Id[i]=ran;
 	}
-	check=sumofAllPlaces(IDNumber);
-	IDNumber[9]=check+48;
-	cout<<IDNumber<<endl;
+	check=sumofAllPlaces(Id);
+	Id[9]=check+47;
+	cout<<"Generated a valid ID #:"<<Id<<endl;
 
 }
--- makefile
+++ makefile
@@ -1,7 +1,5 @@
 
 main:i.o
-	##g++ -o main id.o
-	##g++ -c id.cpp
 	g++ -o test  i.o
 	g++  -g -c  i.cpp
 
